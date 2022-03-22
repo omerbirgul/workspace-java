081b9a5515fc85fb509d7dffaa6769bd1efc5088
Omer Faruk Birgul <omerbirgul1@hotmail.com>
2022-03-22 19:57:50 +0300

    Add | mükemmel sayılar

---
+++ mukemmelSayilar.java
@@ -0,0 +1,29 @@
+
+import java.util.Scanner;
+
+
+public class Main {
+
+   
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter a number");
+        int number = scanner.nextInt();
+        int total = 0;
+        
+        for(int i = 1; i < number; i++){
+            
+            if(number % i == 0){
+                total += i;
+            }    
+        }
+        if(total == number){
+            System.out.println(" Number is perfect number");
+        }
+        else{
+            System.out.println("Number is NOT perfect number");
+        }
+     
+    }
+    
+}
