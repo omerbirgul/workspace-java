8d6a0819c588e39e3ead4b5c2e27061613e9b17a
Omer Faruk Birgul <omerbirgul1@hotmail.com>
2022-10-15 23:09:26 +0300

    Add_metodlarda_parametre

---
+++ 08-methodlarda_Parametre .java
@@ -0,0 +1,15 @@
+
+import java.util.Scanner;
+
+public class NewClass {
+    
+    public static void toplama(int a, int b, int c) {
+        System.out.println("Toplamları = "+ (a+b+c));
+  }
+    
+    public static void main(String[] args) {
+     
+        toplama(0, 3, 5);
+        
+    }
+}
