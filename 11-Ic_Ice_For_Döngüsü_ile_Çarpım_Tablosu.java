a232e5134761e32b92ec7d12d8561d2312ba6df8
Omer Faruk Birgul <omerbirgul1@hotmail.com>
2022-10-18 21:23:54 +0300

    Add_for_dongusu_carpim_tablosu

---
+++ 11-Ic_Ice_For_Döngüsü_ile_Çarpım_Tablosu.java
@@ -0,0 +1,18 @@
+
+public class Çarpım_Tablosu {
+    public static void main(String[] args) {
+        
+        
+        for (int a = 1; a <= 10; a++){
+            
+            for(int b = 1; b <= 10; b++){
+                System.out.println( a + "x" +b+ "=" +(a*b));
+                
+            }
+          System.out.println("******************");
+           System.out.println("******************");
+        }
+        
+    }
+    
+}
